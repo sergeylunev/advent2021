830b3eecf07f26664e4b81bf482c769134ba6013
Sergey Lunev <lunev.s.l@yandex.ru>
2021-12-18 17:04:17 +0300

    Create board from input data
    Check if there is number on board
    
    Green tests

--- src/Service/Bingo/BingoBoard.php
+++ src/Service/Bingo/BingoBoard.php
@@ -4,4 +4,27 @@ namespace Advent\Service\Bingo;
 
 class BingoBoard
 {
+    private array $numbers;
+
+    public function __construct(array $input)
+    {
+        $result = [];
+
+        foreach ($input as $string) {
+            $numbers = explode (' ', $string);
+            foreach ($numbers as $num) {
+                if ($num === '') {
+                    continue;
+                }
+                $result[] = (int) $num;
+            }
+        }
+
+        $this->numbers = $result;
+    }
+
+    public function hasNumber(int $search): bool
+    {
+        return in_array($search, $this->numbers);
+    }
 }
