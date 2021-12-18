93e9bcf91f9c7bad73ef0bcecf86c72a85d573e9
Sergey Lunev <lunev.s.l@yandex.ru>
2021-12-18 16:38:42 +0300

    Start day 4
    Create bingo service test
    Create bingo board test
    
    All is red

---
+++ src/Command/Day4Command.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Advent\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+class Day4Command extends Command
+{
+}
---
+++ tests/Service/Bingo/BingoBoardTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Advent\Tests\Service\Bingo;
+
+use PHPUnit\Framework\TestCase;
+
+class BingoBoardTest extends TestCase
+{
+    public function testCreateBoard()
+    {
+        $input = [
+            '22 13 17 11  0',
+            ' 8  2 23  4 24',
+            '21  9 14 16  7',
+            ' 6 10  3 18  5',
+            ' 1 12 20 15 19',
+        ];
+
+        $board = new BingoBoard($input);
+
+        $this->assertInstanceOf(BingoBoard::class, $board);
+    }
+}
